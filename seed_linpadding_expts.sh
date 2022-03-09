ad4bb8f54ff96f8b1c14fb301c4b6ffcb5ec59bb
chenghuz <chenghuz@andrew.cmu.edu>
2022-03-08 21:53:39 -0500

    linear

---
+++ seed_linpadding_expts.sh
@@ -0,0 +1,22 @@
+python run.py vae3linear_gaussian_12dim2 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 9 -dd 3 --num_batches 100000  --epsilon -1 -tdv -ds 2  -lr 1e-3
+python run.py vae3linear_gaussian_20dim2 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 17 -dd 3 --num_batches 100000  --epsilon -1 -tdv -ds 2  -lr 1e-3
+python run.py vae6linear_gaussian_12dim2 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 6 -dd 6 --num_batches 100000  --epsilon -1 -tdv -ds 2  -lr 1e-3
+python run.py vae6linear_gaussian_20dim2 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 14 -dd 6 --num_batches 100000   --epsilon -1 -tdv -ds 2  -lr 1e-3
+python run.py vae9linear_gaussian_12dim2 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 3 -dd 9 --num_batches 100000   --epsilon -1 -tdv -ds 2  -lr 1e-3
+python run.py vae9linear_gaussian_20dim2 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 10  --padding_dim 11 -dd 9 --num_batches 100000   --epsilon -1 -tdv -ds 2  -lr 1e-3
+python run.py vae12linear_gaussian_20dim2 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 10  --padding_dim 8 -dd 12 --num_batches 100000   --epsilon -1 -tdv -ds 2  -lr 1e-3
+python run.py vae3linear_gaussian_12dim3 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 9 -dd 3 --num_batches 100000  --epsilon -1 -tdv -ds 3  -lr 1e-3
+python run.py vae3linear_gaussian_20dim3 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 17 -dd 3 --num_batches 100000  --epsilon -1 -tdv -ds 3  -lr 1e-3
+python run.py vae6linear_gaussian_12dim3 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 6 -dd 6 --num_batches 100000  --epsilon -1 -tdv -ds 3  -lr 1e-3
+python run.py vae6linear_gaussian_20dim3 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 14 -dd 6 --num_batches 100000   --epsilon -1 -tdv -ds 3  -lr 1e-3
+python run.py vae9linear_gaussian_12dim3 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 3 -dd 9 --num_batches 100000   --epsilon -1 -tdv -ds 3  -lr 1e-3
+python run.py vae9linear_gaussian_20dim3 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 10  --padding_dim 11 -dd 9 --num_batches 100000   --epsilon -1 -tdv -ds 3  -lr 1e-3
+python run.py vae12linear_gaussian_20dim3 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 10  --padding_dim 8 -dd 12 --num_batches 100000   --epsilon -1 -tdv -ds 3  -lr 1e-3
+python run.py vae3linear_gaussian_12dim4 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 9 -dd 3 --num_batches 100000  --epsilon -1 -tdv -ds 4  -lr 1e-3
+python run.py vae3linear_gaussian_20dim4 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 17 -dd 3 --num_batches 100000  --epsilon -1 -tdv -ds 4  -lr 1e-3
+python run.py vae6linear_gaussian_12dim4 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 6 -dd 6 --num_batches 100000  --epsilon -1 -tdv -ds 4  -lr 1e-3
+python run.py vae6linear_gaussian_20dim4 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 14 -dd 6 --num_batches 100000   --epsilon -1 -tdv -ds 4  -lr 1e-3
+python run.py vae9linear_gaussian_12dim4 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 20  --padding_dim 3 -dd 9 --num_batches 100000   --epsilon -1 -tdv -ds 4  -lr 1e-3
+python run.py vae9linear_gaussian_20dim4 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 10  --padding_dim 11 -dd 9 --num_batches 100000   --epsilon -1 -tdv -ds 4  -lr 1e-3
+python run.py vae12linear_gaussian_20dim4 --dataset linear_gaussian --encoder_layer_sizes "" --layer_sizes "" -ow --latent_dim 10  --padding_dim 8 -dd 12 --num_batches 100000   --epsilon -1 -tdv -ds 4  -lr 1e-3
+
